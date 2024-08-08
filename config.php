cb608d710d76267f01de05d3792d171d420ae482
udtcevent <udtcevent@gmail.com>
2024-08-08 16:05:48 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,5 @@
+define('GOOGLE_CLIENT_ID', '746515951882-glui3k5rckb7uapa0pm44rpl6hv4vthm.apps.googleusercontent.com');
+define('GOOGLE_CLIENT_SECRET', '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');
+define('GOOGLE_OAUTH_SCOPE', 'https://www.googleapis.com/auth/calendar');
+// define('REDIRECT_URI', 'http://localhost/e-announce/ggc/google_calendar_event_sync.php');
+define('REDIRECT_URI', 'https://ggc-service-f8b2a7b65e09.herokuapp.com/google_calendar_event_sync.php');
